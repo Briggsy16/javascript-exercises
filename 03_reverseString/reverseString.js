ad72cb76fca375a0549781e17d3a7277eb3ac39b
Matt Briggs <mattbriggs@outlook.com>
2022-10-13 20:30:43 +0100

    exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let length = str.length;
+    let reversedString = "";
+    for (let i = 1; i <= length; i++) {
+        reversedString += str.charAt(length - i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
