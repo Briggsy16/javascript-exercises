cea2284c1f3765a7dc1ed9f8d9be17f7fcdf2ba4
Matt Briggs <mattbriggs@outlook.com>
2022-10-13 21:25:00 +0100

    exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, value) {
+    const args = [];
+    array = Array.from(arguments[0]);
 
+    for (let i = 1; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
+
+    for (let i = 0; i < args.length; i++) {
+        let valueToRemove = args[i];
+        if (array.includes(valueToRemove)) {
+            array.splice(array.indexOf(valueToRemove), 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
