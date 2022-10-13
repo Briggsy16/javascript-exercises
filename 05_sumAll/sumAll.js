e58ff55ce971414b435f4a676d087c8b09b89a68
Matt Briggs <mattbriggs@outlook.com>
2022-10-13 21:36:12 +0100

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+    if (
+        typeof startNum != "number" ||
+        typeof endNum != "number" ||
+        startNum < 0 ||
+        endNum < 0
+    ) {
+        return "ERROR";
+    }
 
+    let difference = endNum - startNum;
+    if (difference < 0) {
+        let tempNum = endNum;
+        endNum = startNum;
+        startNum = tempNum;
+    }
+
+    let sum = 0;
+
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
