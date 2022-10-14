f77092e468485e427f3df17d9d485b39ece454fb
Matt Briggs <mattbriggs@outlook.com>
2022-10-14 15:16:44 +0100

    exercise 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temperature) {
+    let celcius = (temperature - 32) * 0.5556;
+    return parseFloat(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (temperature) {
+    let farenheit = temperature * 1.8 + 32;
+    return parseFloat(farenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
